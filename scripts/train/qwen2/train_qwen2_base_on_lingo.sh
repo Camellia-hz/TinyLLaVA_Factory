87b38a6aa922907c5c1bd4b5de97b4d659079152
Camellia-hz <z_baishancha@126.com>
2025-01-26 12:34:01 +0000

    update scripts

--- scripts/train/qwen2/train_qwen2_base_on_lingo.sh
+++ scripts/train/qwen2/train_qwen2_base_on_lingo.sh
@@ -1,11 +1,11 @@
 FINETUNE_DATA_PATH=playground/data/LingoQA/train_multi.json #finetune annotation file path
 FINETUNE_IMAGE_PATH=""
-LLM_VERSION=/mnt/csi-data-aly/user/haozhou/Projects/TinyLLaVA_Factory/checkpoints/Qwen/Qwen2.5-3B # llm path in huggingface
+LLM_VERSION=/mnt/csi-data-aly/user/haozhou/Projects/TinyLLaVA_Factory/checkpoints/Qwen/Qwen2-0.5B # llm path in huggingface
 VT_VERSION=/mnt/csi-data-aly/shared/public/haozhou/checkpoints/siglip/siglip-so400m-patch14-384/ #vision tower path in huggingface
 VT_VERSION2="" #if you are not using mof vision tower, keep it empty
 CN_VERSION=mlp2x_gelu #connector type, other options are: qformer, resampler, etc
 CONV_VERSION=qwen2_base #chat template, other options are: phi, llama, gemmma, etc
-VERSION=qwen2.5-3b_base #experiment name for recording different runnings
+VERSION=qwen2-0.5b_base #experiment name for recording different runnings
 TRAIN_RECIPE=common #training recipes, other options are: lora, qlora
 MODEL_MAX_LENGTH=8192 #max model length for llm
 
